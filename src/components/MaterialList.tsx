9984a6d9952a496160465d1d8f782f4bf56eb6ee
qwesmayn <109741981+qwesmayn@users.noreply.github.com>
2024-06-30 22:31:10 +0300

    fix

--- src/components/MaterialList.tsx
+++ src/components/MaterialList.tsx
@@ -6,7 +6,10 @@ import { ITypesMaterials } from "../models/ITypesMaterials";
 import MaterialBlock from "./MaterialBlock";
 import StudentMaterialBlock from "./StudentMaterialBlock";
 import { useAppDispatch, useAppSelector } from "../hooks/typeHooks";
-import { getDisciplinesOnIdGroups, getStudents } from "../store/action_creators/actionCreatos";
+import {
+  getDisciplinesOnIdGroups,
+  getStudents,
+} from "../store/action_creators/actionCreatos";
 import { IUser } from "../models/IUser";
 import { jwtDecode } from "jwt-decode";
 import Loading from "./Loadind";
@@ -31,15 +34,19 @@ const MaterialsList: FC<MaterialsListProps> = ({
   isStudentView = false,
 }) => {
   const dispatch = useAppDispatch();
-  const { students, isLoading: studentsLoading } = useAppSelector((state) => state.userManageReducer);
-  const { groupsById, isLoading: groupsLoading } = useAppSelector((state) => state.groupsByIdReducer);
+  const { students, isLoading: studentsLoading } = useAppSelector(
+    (state) => state.userManageReducer
+  );
+  const { groupsById, isLoading: groupsLoading } = useAppSelector(
+    (state) => state.groupsByIdReducer
+  );
   const [userId, setUserId] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true); // State to manage loading state
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    dispatch(getStudents());
     if (isStudentView) {
-      const token = localStorage.getItem('token');
+      dispatch(getStudents());
+      const token = localStorage.getItem("token");
       if (token) {
         const decodedToken = jwtDecode<IUser>(token);
         setUserId(decodedToken.id);
@@ -49,50 +56,60 @@ const MaterialsList: FC<MaterialsListProps> = ({
 
   useEffect(() => {
     if (isStudentView && userId && students.length > 0) {
-      const user = students.find(student => student._id === userId);
+      const user = students.find((student) => student._id === userId);
       if (user) {
         dispatch(getDisciplinesOnIdGroups(user.group._id));
       }
+      setLoading(true);
     }
   }, [isStudentView, userId, students, dispatch]);
 
   useEffect(() => {
-    // Check if both students and groupsById are loaded and filteredMaterials can be populated
-    if (!studentsLoading && !groupsLoading && userId && groupsById.length > 0) {
-      setLoading(false); // Set loading to false when everything is ready
+    if (
+      isStudentView &&
+      userId &&
+      !studentsLoading &&
+      !groupsLoading &&
+      groupsById.length > 0
+    ) {
+      setLoading(false);
     }
-  }, [studentsLoading, groupsLoading, userId, groupsById]);
+  }, [isStudentView, userId, studentsLoading, groupsLoading, groupsById]);
 
-  const filteredMaterials = isStudentView && !loading
-    ? materials.filter(material =>
-        groupsById.some(groupById => groupById.discipline._id === material.discipline)
-      )
-    : [];
-
-  return (
-    loading ? <Loading /> :
-    <div className="flex pl-9 flex-wrap gap-20">
-      {filteredMaterials.map((material) =>
-        isStudentView ? (
-          <StudentMaterialBlock
-            key={material._id}
-            material={material}
-            authors={authors}
-            disciplines={disciplines}
-            materialsTypes={materialsTypes}
-          />
-        ) : (
-          <MaterialBlock
-            key={material._id}
-            material={material}
-            authors={authors}
-            disciplines={disciplines}
-            materialsTypes={materialsTypes}
-            onSave={onSave}
-            openModal={openModal}
-          />
+  const filteredMaterials =
+    isStudentView && !loading
+      ? materials.filter((material) =>
+          groupsById.some(
+            (groupById) => groupById.discipline._id === material.discipline
+          )
         )
-      )}
+      : [];
+
+  return loading ? (
+    <Loading />
+  ) : (
+    <div className="flex pl-9 flex-wrap gap-20">
+      {isStudentView
+        ? filteredMaterials.map((material) => (
+            <StudentMaterialBlock
+              key={material._id}
+              material={material}
+              authors={authors}
+              disciplines={disciplines}
+              materialsTypes={materialsTypes}
+            />
+          ))
+        : materials.map((material) => (
+            <MaterialBlock
+              key={material._id}
+              material={material}
+              authors={authors}
+              disciplines={disciplines}
+              materialsTypes={materialsTypes}
+              onSave={onSave}
+              openModal={openModal}
+            />
+          ))}
     </div>
   );
 };
